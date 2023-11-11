6918c4b0f32186fd12fdd1225c74d034a3213c9e
retRodya <rodion8999@gmail.com>
2023-11-11 17:46:52 +0300

    Have deleted all/Add Interview

--- Courses/UDEMYJavaDlyaNachS0DoSertOrcl/src/L27_Exceptions/Homework/NotWaterException.java
+++
@@ -1,8 +0,0 @@
-package L27_Exceptions.Homework;
-
-public class NotWaterException extends Exception{
-    NotWaterException(String notWaterStringParameter) {
-        super(notWaterStringParameter);
-    }
-}
-
