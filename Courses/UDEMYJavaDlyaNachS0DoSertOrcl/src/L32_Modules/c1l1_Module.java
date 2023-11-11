9c7ddf1de975db53f0cb77375fbb06aa744bc097
retRodya <rodion8999@gmail.com>
2023-11-11 17:21:51 +0300

    have deleted all except Curses

--- Courses/UDEMYJavaDlyaNachS0DoSertOrcl/src/L32_Modules/c1l1_Module.java
+++ Courses/UDEMYJavaDlyaNachS0DoSertOrcl/src/L32_Modules/c1l1_Module.java
@@ -1,8 +1,17 @@
 package L32_Modules;
 
 /**
- * Module - is a group of closely interconnected packages.
- * [package is
+ * Module - is a group of closely interconnected packages and resources.
+ * [package is collection of related classes and interfaces provides
+ * access protection and namespaces management]
+ * [resources - are collections of images, txt files and etc. which necessary for
+ * correct compiling and work]
+ *
+ * Advantages:
+ * 1. Module programming require less space for application.
+ * 2. Additional encapsulation at module level.
+ * 3. Improved management by module descriptor.
+ *
  */
 
 public class c1l1_Module {
