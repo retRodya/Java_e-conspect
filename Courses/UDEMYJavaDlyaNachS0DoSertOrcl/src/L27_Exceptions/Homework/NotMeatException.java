a2b9486308c114146d3f88113dfcebbbfa6b26ef
retRodya <rodion8999@gmail.com>
2023-11-11 17:15:33 +0300

    Amend/LearningGitFromIntelijIDEA

--- Courses/UDEMYJavaDlyaNachS0DoSertOrcl/src/L27_Exceptions/Homework/NotMeatException.java
+++
@@ -1,7 +0,0 @@
-package L27_Exceptions.Homework;
-
-public class NotMeatException extends RuntimeException {
-    NotMeatException(String notMeatStringParameter) {
-        super(notMeatStringParameter);
-    }
-}
--- Courses/UDEMYJavaDlyaNachS0DoSertOrcl/src/L31_Generics/c3l3_TypeErasure.java
+++
@@ -1,5 +0,0 @@
-package L31_Generics;
-
-public class c3l3_TypeErasure {
-    // see c1l1
-}
