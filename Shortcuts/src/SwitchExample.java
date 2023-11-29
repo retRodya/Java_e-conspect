0463926c5cb30f8b710db2cfed4011612092bc18
Radzivon/retRodya <rodion.bukhtarevich@gmail.com>
2023-11-29 16:02:02 +0300

    add switch example

---
+++ Shortcuts/src/SwitchExample.java
@@ -0,0 +1,15 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int value = 11;
+        switch (value) {
+            case 1:
+                System.out.println("value = 1");
+                break;
+            case 10:
+                System.out.println("value = 10");
+                break;
+            default:
+                System.out.println("value N/A");
+        }
+    }
+}
