b374eb9920821cd6976cce1513043fc0927e0957
Radzivon/retRodya <rodion.bukhtarevich@gmail.com>
2023-11-29 16:18:09 +0300

    new branch Tasks from master/add MonthNumber_Switch task

---
+++ Tasks/src/MonthNumber_Switch.java
@@ -0,0 +1,25 @@
+/**
+ * Number of month given. Show month name.
+ */
+public class MonthNumber_Switch {
+    static int monthNumber;
+
+    public static void main(String[] args) {
+        monthNumber = 12;
+
+        switch (monthNumber) {
+            case 1 -> System.out.println("January");
+            case 2 -> System.out.println("February");
+            case 3 -> System.out.println("March");
+            case 4 -> System.out.println("April");
+            case 5 -> System.out.println("May");
+            case 6 -> System.out.println("June");
+            case 7 -> System.out.println("July");
+            case 8 -> System.out.println("August");
+            case 9 -> System.out.println("September");
+            case 10 -> System.out.println("October");
+            case 11 -> System.out.println("November");
+            case 12 -> System.out.println("December");
+        }
+    }
+}
