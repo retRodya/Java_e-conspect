f0ef4efec1a022eca486e5efb90bb2b1e6090d6b
Radzivon/retRodya <rodion.bukhtarevich@gmail.com>
2024-01-23 22:24:44 +0300

    DMDev courses

---
+++ Courses/UDEMYDMDevJavaDlyaNachLvl2/src/L05_14_OOP_Enum/MYTestTask/c2l14_MYTestTaskRunner.java
@@ -0,0 +1,9 @@
+package L05_14_OOP_Enum.MYTestTask;
+
+public class c2l14_MYTestTaskRunner {
+    public static void main(String[] args) {
+        e4l14_MYEnumTestTask_Seasons winter = e4l14_MYEnumTestTask_Seasons.WINTER;
+
+        System.out.println(winter.getDescription());
+    }
+}
